5ce95ab025fd6b5d2f2687db6b4203d71a99c8ca
Jim Jones <jim.jones@uni-muenster.de>
2018-10-18 18:09:40 +0200

    Adds usage example

--- musicowl/src/main/java/de/wwu/music2rdf/example/Example.java
+++ musicowl/src/main/java/de/wwu/music2rdf/example/Example.java
@@ -11,7 +11,7 @@ public class Example {
 	public static void main(String[] args) {
 				
 		MusicXML2RDF music2rdf = new MusicXML2RDF();
-		
+			
 		music2rdf.setInputFile(new File("musicxml/elgar_cello_concerto_op.85.xml"));
 		music2rdf.setOutputFile("ntriples/elgar_cello_concerto_op.85.nt");
 		music2rdf.setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Elgar-cello-concerto-manuscript.jpg/220px-Elgar-cello-concerto-manuscript.jpg");
