59455d70811a91516148f8f219da0b48cd3f44f4
Jim Jones <jimjonesbr@gmail.com>
2019-06-21 12:47:16 +0200

    Change pom.xml for Junit tests

--- musicowl/src/test/de/www/music2rdf/junit/TestConverter.java
+++ musicowl/src/test/de/www/music2rdf/junit/TestConverter.java
@@ -3,7 +3,6 @@ package de.www.music2rdf.junit;
 import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
-
 import de.wwu.music2rdf.util.Util;
 
 public class TestConverter {
@@ -11,6 +10,12 @@ public class TestConverter {
 	public void testTimeElapsed() {
 		System.out.println("Executing tests ...");
 		assertEquals(Util.timeElapsed(new Date(), new Date()), "0 ms");
-
+		
 	}
+	@Test
+	public void testSum() {
+		System.out.println("Executing tests (Sum)...");
+		assertEquals(2+2, 4);		
+	}
+	
 }
