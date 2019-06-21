b639048748eccaed70a552c99b4b442edcb32d2e
Jim Jones <jimjonesbr@gmail.com>
2019-06-21 12:21:19 +0200

    Add log for JUnit test

--- musicowl/src/main/tests/musicowl/TestConverter.java
+++ musicowl/src/main/tests/musicowl/TestConverter.java
@@ -9,7 +9,7 @@ import de.wwu.music2rdf.util.Util;
 public class TestConverter {
 	@Test
 	public void testTimeElapsed() {
-
+		System.out.println("Executing tests ...");
 		assertEquals(Util.timeElapsed(new Date(), new Date()), "0 ms");
 
 	}
