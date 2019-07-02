8f30a3b85360297d5d49dcf7533c877cad6447f3
Jim Jones <jim.jones@uni-muenster.de>
2019-07-02 13:55:21 +0200

    Add example class

--- musicowl/src/main/java/de/wwu/music2rdf/example/Example.java
+++ musicowl/src/main/java/de/wwu/music2rdf/example/Example.java
@@ -1,7 +1,5 @@
 package de.wwu.music2rdf.example;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 import de.wwu.music2rdf.converter.MusicXML2RDF;
 import de.wwu.music2rdf.core.Collection;
@@ -18,7 +16,7 @@ public class Example {
 		music2rdf.setOutputFile("ntriples/elgar_cello_concerto_op.85.nt");
 		music2rdf.setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Elgar-cello-concerto-manuscript.jpg/220px-Elgar-cello-concerto-manuscript.jpg");
 		music2rdf.setScoreURI("http://dbpedia.org/resource/Cello_Concerto_(Elgar)");
-		music2rdf.addCollection(new Collection("https://url.collection.de","Great Composers"));
+		music2rdf.addCollection(new Collection("https://wwu.greatcomposers.de","Great Composers"));
 		music2rdf.addPerson(new Person("http://dbpedia.org/resource/Edward_Elgar","Sir Edward William Elgar",Role.COMPOSER));
 		music2rdf.addPerson(new Person("http://jimjones.de","Jim Jones",Role.ENCODER));		
 		music2rdf.setDocumentTitle("Cellokonzert e-Moll op. 85");
@@ -27,26 +25,6 @@ public class Example {
 		music2rdf.setDateIssued("1919"); //Formats accepted: yyyy, yyyyMM, yyyyMMdd.
 		
 		music2rdf.parseMusicXML();
-	
-		
-		
-				
-		music2rdf.setInputFile(new File("musicxml/sonate_facile_piano-forte.xml"));
-		music2rdf.setOutputFile("src/test/resources/rdf/sonate_facile_piano-forte");
-		music2rdf.setThumbnail("http://sammlungen.ulb.uni-muenster.de/download/webcache/304/5109295");
-		music2rdf.setScoreURI("https://sammlungen.ulb.uni-muenster.de/id/5109291");
-		music2rdf.addCollection(new Collection("https://sammlungen.ulb.uni-muenster.de","Digitale Sammlungen der Universität und Landesbibliothek Münster"));
-		music2rdf.addPerson(new Person("http://d-nb.info/gnd/129246638","Bečvařovský, Antonín František",Role.COMPOSER));
-		music2rdf.addPerson(new Person("http://jimjones.de","Jim Jones",Role.ENCODER));		
-		music2rdf.setDocumentTitle("Sonate facile pour Piano-Forte");
-		music2rdf.setDateIssued("1800"); 
-		music2rdf.setOutputFormat("TURTLE");
-		music2rdf.isVerbose(false);
-		
-		music2rdf.parseMusicXML();
-		
-		File file = new File("src/test/resources/rdf/sonate_facile_piano-forte.ttl");
-		assertEquals(true, file.exists());
 	}
 
 }
